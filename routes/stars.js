191f7a0edb12ecc839bd05d43bf54b146157aa7d
Renell Castro <rcskca@gmail.com>
2019-03-29 04:50:44 -0600

    Added route to check for query param range or std

--- routes/stars.js
+++ routes/stars.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req ,res) => {
+    let offset = Math.round(parseInt(req.query.offset) / 10) * 10;
+
+    if (req.query.range || req.query.std) {
+        res.send(`Getting stars with an offset of ${offset} and with a ${req.query.range ? 'range' : 'standard deviation'} sort.`);
+    } else {
+        res.send(`Getting stars with an offset of ${offset}.`);
+    }
+});
+
 router.get('/:starId', (req, res) => {
     res.send(`Requesting star data for star ${req.params.starId}`);
 });
