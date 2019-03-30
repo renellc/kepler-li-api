676e4a5543bdf64f55250c0a4d5af8702482e4b6
Renell Castro <rcskca@gmail.com>
2019-03-30 00:19:39 -0600

    commented some lines out for testing

--- routes/stars.js
+++ routes/stars.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req ,res) => {
-    let offset = Math.round(parseInt(req.query.offset) / 10) * 10;
+router.get('/', (req, res) => {
+    //let offset = Math.round(parseInt(req.query.offset) / 10) * 10;
 
-    if (req.query.range || req.query.std) {
-        res.send(`Getting stars with an offset of ${offset} and with a ${req.query.range ? 'range' : 'standard deviation'} sort.`);
-    } else {
-        res.send(`Getting stars with an offset of ${offset}.`);
-    }
+    // if (req.query.range || req.query.std) {
+    //     res.send(`Getting stars with an offset of ${offset} and with a ${req.query.range ? 'range' : 'standard deviation'} sort.`);
+    // } else {
+    //     res.send(`Getting stars.`);
+    // }
+
+    res.send(`Getting stars.`);
 });
 
 router.get('/:starId', (req, res) => {
